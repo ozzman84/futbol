85ccf1056200c16a840a64b03bd7cb0e2deaad68
Ozzie <mikeosmonson@gmail.com>
2021-07-15 11:44:17 -0600

    Add CSV files to Class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,2 +1,24 @@
+require 'CSV'
+
 class StatTracker
-end 
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize(locations)
+    @games      = locations[:games]
+    @teams      = locations[:teams]
+    @game_teams = locations[:game_teams]
+  end
+
+  def generate_stats(location, object)
+    csv = CSV.read(location, headers: true)
+    x = csv.map do |row|
+      object.new(row)
+    end
+  end
+
+  def highest_total_score
+
+  end
+end
